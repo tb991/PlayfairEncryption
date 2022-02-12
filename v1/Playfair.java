79ed5f27a5a6432d285f6cfd9ede9380f6bca296
tom <tomburgess24@gmail.com>
2022-02-12 21:18:29 +0000

    debugging improvement

--- v1/Playfair.java
+++ v1/Playfair.java
@@ -1,18 +1,24 @@
 class Playfair {
-	public Playfair(){
+	char[][] key;
+	public Playfair(String keyGenText){
 			
 	}
 	public static void main(String[] args){
 		printy();
+		print("\n");
 		keyprint("ABCDEFGHIKLMNOPQRSTUVWXYZ");
+		print("\n");
 		keyprint("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+		print("\n");
+		keyGen("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+		print("\n");
 	}
 	private static void print(Object l){
 		System.out.print(String.valueOf(l));
 	}
 	private static void printy(){
-		short len = 6;
-		short count = 0;
+		int len = 6;
+		int count = 0;
 		String spacing = " ";
 		while (count < len*len){
 			print(count+1);
@@ -26,25 +32,47 @@ class Playfair {
 			}
 		}
 	}
-	private static void keyprint(String k){
-		short keyW = 0;
-		while (keyW*keyW < k.length()){
+	private static int sqrt(int in){
+		int keyW = 0;
+		while (keyW*keyW <= in){
 			keyW++;
 		}
 		keyW--;
-		short len = 6;
-		short count = 0;
+		return keyW;
+	}
+	private static void keyprint(String k){
+		int keyW = sqrt(k.length());
+		int count = 0;
 		String spacing = " ";
-		short it = 0;
+		int it = 0;
 		while (it < k.length()){
 			print(k.charAt(it));
 			it++;
 			print(spacing);
 			count++;
-			if (count%(keyW+1)==0){
+			if (count%(keyW)==0){
 				print("\n");
 			}
 		}
+	}
+	private static char[][] keyGen(String k){
+		int keyW = sqrt(k.length());
+		int count = 0;
+		String spacing = " ";
+		int it = 0;
+		char[][] ke = new char[keyW][keyW];
+		char now;
+		while (it < k.length()){
+			now = k.charAt(it);
+			ke[it%keyW][it/keyW]=now;
+			print(ke[it%keyW][it/keyW]);
+			it++;
+			count++;
+			if (count%(keyW)==0){
+				print("\n");
+			}
+		}
+		return ke;
 	}
 	
 }
