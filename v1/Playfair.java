bfb4f98e5c37e014ba19002bef713c7b87b60897
tom <tomburgess24@gmail.com>
2022-02-13 01:57:56 +0000

    cleaner output for key class

--- v1/Playfair.java
+++ v1/Playfair.java
@@ -1,9 +1,41 @@
 class Playfair {
-	char[][] key;
-	public Playfair(String keyGenText){
-			
+	static char[][] key;
+	static String alpha; // alphabet
+	static int len;
+	static String txt;
+	public Playfair(String text, String alphabet){
+		alpha = removeRepeats(alphabet);
+		txt = removeRepeats(text);
+		String keyString = removeRepeats(txt+alpha);
+		print(keyString);
+		try{
+			if (checkSqr(keyString.length())){
+				
+			}
+			else{
+				throw new Exception("Key not square length");
+			}
+		}
+		catch(Exception ex){
+			print(ex.getMessage());
+			return; // not sure if needed
+		}
+		keyGen(keyString);
+		len = keyString.length();
+		printActualKey();
 	}
 	public static void main(String[] args){
+		Playfair pf;
+		pf = new Playfair("honeycomb", "abcdefghiklmnopqrstuvwxyz");
+	}
+	private static boolean checkSqr(int in){
+		boolean out = false;
+		if (Math.sqrt(in) == sqrt(in)){
+			out = true;
+		}
+		return out;
+	}
+	private static void tests(){
 		printy();
 		print("\n");
 		keyprint("ABCDEFGHIKLMNOPQRSTUVWXYZ");
@@ -18,7 +50,12 @@ class Playfair {
 		print("\n");
 		keyprint("gergeronimoggeronimoerongeronimoimoonigeronimomo");
 		print("\n");
+		keyGen("12345678912345678912345678922123456789123456789123456789123456789123456789123456789");
+		print("\n");
+		print(removeEntries('s', "sssssssssshsssssissssssss"));
+		print("\n");
 	}
+	
 	// removes repeated characters from a string
 	private static String removeRepeats(String s){
 		String out = "";
@@ -43,6 +80,22 @@ class Playfair {
 	}
 	private static void print(Object l){
 		System.out.print(String.valueOf(l));
+	}
+	public static void printActualKey(){
+		print("\n");
+		int keyW = sqrt(len);
+		int it = 0;
+		int bouncer = 0;
+		while (it < keyW*keyW){
+			print(key[it%keyW][it/keyW]);
+			print(" ");
+			it++;
+			bouncer++;
+			if (bouncer==keyW){
+				bouncer = 0;
+				print("\n");
+			}
+		}
 	}
 	// helps with key generation alg
 	private static void printy(){
@@ -86,25 +139,57 @@ class Playfair {
 			}
 		}
 	}
-	// playfair key generation with any length, but without repeat detection
+	// playfair key generation, but without repeat detection, and errs with bad lengths
 	private static char[][] keyGen(String k){
 		int keyW = sqrt(k.length());
+		try{
+			if (keyW*keyW!=k.length()){
+				throw new Exception("key length not square");
+			}
+		}
+		catch (Exception ex){
+			print(ex.getMessage());
+			return null;
+		}
 		int count = 0;
-		String spacing = " ";
 		int it = 0;
 		char[][] ke = new char[keyW][keyW];
 		char now;
 		while (it < k.length()){
 			now = k.charAt(it);
 			ke[it%keyW][it/keyW]=now;
-			print(ke[it%keyW][it/keyW]);
+			//print(ke[it%keyW][it/keyW]);
 			it++;
 			count++;
 			if (count%(keyW)==0){
-				print("\n");
+				//print("\n");
 			}
+			
 		}
+		key = ke.clone();
 		return ke;
 	}
+	// remove elements in s from actonme
+	private static String removeEntries(char c, String s){
+		int i;
+		i = 0;
+		char d;
+		String out = "";
+		boolean found = false;
+		int idx;
+		while (i < s.length()){
+			d = s.charAt(i);
+			if (c==d){
+				found = true;// found char
+			}
+			if (!found){
+				out += s.charAt(i);
+			}
+			found = false;
+			i++;
+		}
+		return out;
+	}
+	
 	
 }
